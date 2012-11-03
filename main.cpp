1a7984bcf51afa4626d288912aba5c385f80c3d8
Andrew <accraze@gmail.com>
2012-11-02 22:23:19 -0700

    battle loop updated

--- main.cpp
+++ main.cpp
@@ -3,6 +3,26 @@
 
 using namespace std;
 
+//Declaring all Global Variables
+int turnCount=1, gameInput=0, fireHere = 0, j=0;	
+char mainInput;
+Battleship* pShip1 = NULL;
+Destroyer* pShip2 = NULL;
+Cruiser* pShip3 = NULL;
+Carrier* pShip4 = NULL;
+Submarine* pShip5 = NULL;
+bool shipMove=true;
+
+enum ShipType {
+	CARRIER,
+	BATTLESHIP,
+	CRUISER,
+	SUBMARINE,
+	DESTROYER
+};
+
+
+// Forward Declarations //
 void menu(); 
 void branching(char option);
 void new_game();
@@ -10,29 +30,19 @@ void view_ranking();
 void programStart();
 void battle_menu();
 void battle_branch(char option);
-void fire();
-void change_location();
-void display_ships();
-void enemy_ships();
 void skip();
 
 
-int main() {
+
+void main() {
 	char ch; 
 	
 	programStart();
-	
-	do {
-		menu();
-		ch = tolower(getchar()); 
-		cin.ignore();
-		branching(ch);
-	} while (ch != 'q');
-	
-	return 0; 
-};
+	startMenu();	
 
-void menu()		//menu displays the menu of options for the user
+}
+
+void main_menu()		//menu displays the menu of options for the user
 {
 	cout << "\n\nMenu Options\n";
 	cout << "------------------------------------------------------\n";
@@ -59,23 +69,33 @@ void programStart()
     cout << "      '  \\   /     /                              \\_________________/ " << endl;
 }
 
+void startMenu(void) {
+
+	mainInput = 0;
+	main_menu();
+	cin>>mainInput;
+	branching(tolower(mainInput));
+
+}
+
 void branching(char option)		
 {
 	switch(option)
 	{
 		case 'n':
-			new_game();
+			new_game();	// start a new game
 			break;
 			
 		case 'v':
-			view_ranking();
+			view_ranking();   // view high scores
 			break;
 			
-		case 'q':
+		case 'q':			// quit game
 			break;
 			
 		default:
 			printf("\nError: Invalid Input.  Please try again..."); 
+			startMenu(); // go back to Main Menu
 			break;
 	}
 }
@@ -84,7 +104,8 @@ void new_game()
 {
 	// needs to have player set the grid sizes
 	// needs to have player place battleships on the grid
-	// then battle will commence
+	turnReset();			//Set the turn counter back to 1.
+	battleSwitch();			//Go to the game menu
 }
 
 void view_ranking()
@@ -92,89 +113,105 @@ void view_ranking()
 	// This needs to read from file and display 10 highest scores in the game 
 }
 
-void battle()
-{
-	char ch; 
-	
-	
-	do {
-		battle_menu();
-		ch = tolower(getchar()); 
-		cin.ignore();
-		battle_branch(ch);
-		// call computer turn NEED TO WRITE THIS CODE!!!!
-	} while (ch != 'q');
-	
-}
 
 void battle_menu()
 {
 	cout << "\nBattle Options\n";
 	cout << "------------------------------------------------------\n";
-	cout << "f: Fire!\n";
-	cout << "c: Change Battleship location\n";
-	cout << "d: Display remaining Battleships\n";
-	cout << "e: Display Enemy's remaining Battleships\n";
-	cout << "s: Skip Turn\n";
-	cout << "q: Quit\n";
-	cout << "\n\nPlease enter a choice (f, c, d, e, s or q) ---> "; 
+	cout << "1: Fire!\n";
+	cout << "2: Change Battleship location\n";
+	cout << "3: Display remaining Battleships\n";
+	cout << "4: Display Enemy's remaining Battleships\n";
+	cout << "5: Skip Turn\n";
+	cout << "6: Quit\n";
+	cout << "\n\nPlease enter a choice (1, 2, 3, 4, 5 or 6) ---> "; 
 	
 }
 
-void battle_branch(char option)
-{
-	switch(option)
-	{
-		case 'f':	// fire
-			fire();
+
+void battleSwitch(void){
+	// case1: player wins the game
+	if (computer->lost()) {
+		victory();
+	  //save player name
+	  // check to see if player got high score
+	}
+	// case2: computer wins the game
+	if (ship1->destroyed() && ship2->destroyed() && ship3->destroyed() && ship4->destroyed() && ship5->destroyed()) {
+		gameOver();
+		// print high scores!!
+	}
+	
+	if()
+	
+	battle_menu();			//Print out the battle menu
+	cin >> gameInput;		//obtain user's input
+	switch (gameInput) {
+		case 1:          // fire at enemy
+			shipMove=true;
+			// fill in firing logic here
 			break;
 			
-		case 'c':	// change location
-			change_location();
+		case 2:          // move ships
+			if(shipMovie){ // checks to see if ship has been moved this turn or not
+			// fill in moving logic
+			}
 			break;
 			
-		case 'd':	// your remaining battleships
-			display_ships();
-			break;
-		case 'e':	// enemy remaining battleships
-			enemy_ships();
+		case 3:          // display the player's ships/health
+			// print out the players remaining ships
+			// print out the players remaing health etc...
+			battleSwitch(); // Go Back to Battle Menu...
 			break;
 			
-		case 's':	// skip turn
+		case 4:          // display the computers ships/health
+			// print out the computers remaining ships
+			battleSwitch();// Go Back to Battle Menu...
+			break;	
+			
+		case 5:          // skip players turn
 			skip();
-			break;
+			turnCount(); // turn counter increment
+			// computers turn
+			battleSwitch();// Go Back to Battle Menu...
+			break;		
 			
-		case 'q':	// quit
+		case 6:          // quit the game
+			// DELETE ALL OBJECTS!!!!!
+			
 			break;
 			
 		default:
-			printf("\nError: Invalid Input.  Please try again..."); 
+			battleSwitch();    // if 1-6 not entered, Go Back To Battle Menu!
 			break;
 	}
-	
-	
-}
-
-void fire(){
-// fires on a square in the grid
-
-// turn counter++ for score
-	
-}
-
-void change_location(){
-// change one battleship location
-}
-
-void display_ships(){
-// display your remaining ships
-}
-
-void enemy_ships(){
-// display enemy remaining ships	
 }
 
 void skip(){
-// skip turn	
+// skip turn
+	cout << endl << endl << endl;
+	cout << "Skipping Player's Turn";
+	cout << endl << endl << endl;
 }
+
+void turnCount(void){	//Increment the turn by 1.
+	turnCount++;
+}
+
+void turnReset(void){	//Increment the turn by 1.
+	turnCount = 1;
+}
+
+void victory(void){
+	cout << "Hurray! You sunk all of the enemy's Battleships";
+	startMenu();
+	
+}
+
+void gameOver(void){
+	cout << "Wah Wah! You suck!";
+	startMenu();
+	
+}
+
 
